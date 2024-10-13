4986fd12ff552551fab7e3a40097f7fe5f45c9d8
ARONLS23 <aronsls20@gmail.com>
2024-10-13 17:56:30 -0500

    proyecto base

---
+++ src/test/java/com/arrows_ls/best_travel/BestTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arrows_ls.best_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
