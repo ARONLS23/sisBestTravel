c9e93c7a36b2996b0aa2780691576e180f776188
ARONLS23 <aronsls20@gmail.com>
2024-11-02 11:37:15 -0500

    configurando registro de cliente

--- src/main/java/com/arrows_ls/best_travel/config/SecurityConfig.java
+++ src/main/java/com/arrows_ls/best_travel/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.arrows_ls.best_travel.config;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
@@ -9,13 +10,33 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
+import java.util.UUID;
+
 @Configuration
 public class SecurityConfig {
+
+    @Value(value = "${app.client.id}")
+    private String clientId;
+    @Value(value = "${app.client.secret}")
+    private String clienteSecret;
+    @Value(value = "${app.client-scope-read}")
+    private String scopeRead;
+    @Value(value = "${app.client-scope-write}")
+    private String scopeWrite;
+    @Value(value = "${app.client-redirect-debugger}")
+    private String redirectUri1;
+    @Value(value = "${app.client-redirect-spring-doc}")
+    private String redirectUri2;
 
     private final UserDetailsService userDetailsService;
 
@@ -57,6 +78,23 @@ public class SecurityConfig {
         authenticationProvider.setPasswordEncoder(encoder);
         authenticationProvider.setUserDetailsService(userDetailsService);
         return authenticationProvider;
+    }
+
+    @Bean
+    public RegisteredClientRepository registeredClientRepository(BCryptPasswordEncoder encoder){
+        var registeredClient = RegisteredClient
+                .withId(UUID.randomUUID().toString())
+                .clientId(clientId)
+                .clientSecret(clienteSecret)
+                .scope(scopeRead)
+                .scope(scopeWrite)
+                .redirectUri(redirectUri1)
+                .redirectUri(redirectUri2)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
     }
 
     @Bean
