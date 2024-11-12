4430894ec9988fa54d3279aea553a054d9dec989
ARONLS23 <aronsls20@gmail.com>
2024-11-12 16:24:58 -0500

    finalizando configuración v1

--- db/mongo/init-mongo.js
+++ db/mongo/init-mongo.js
@@ -18,7 +18,7 @@ db.app_users.insert([
         "password": "$2a$10$VV2WjbpSnKa2RUl0xEcQw.MF2JTQ3N39Suxdgg5p3tTaTyKEOaOsm",
         "role":
             {
-                "granted_authorities": ["read"]
+                "granted_authorities": ["ROLE_USER"]
             }
     },
     {
@@ -28,7 +28,7 @@ db.app_users.insert([
         "password": "$2a$10$tgmS0HU2csPTKbZNi/BF/Op8CEfr46AZk96ml8lQfF0I7l1HYGxB2",
         "role":
             {
-                "granted_authorities": ["read"]
+                "granted_authorities": ["ROLE_USER"]
             }
     },
     {
@@ -38,7 +38,7 @@ db.app_users.insert([
         "password": "$2a$10$d7xyNl5sVh7I4ytZ7mlfqOxStjSl4Irt9LD2U2xdSLP6US39EOhRa",
         "role":
             {
-                "granted_authorities": ["read", "write"]
+                "granted_authorities": ["ROLE_USER", "ROLE_ADMIN"]
             }
     },
     {
@@ -48,7 +48,7 @@ db.app_users.insert([
         "password": "$2a$10$gtnspUdSvGgbNolE1P/AhuLXWhyLo5AmzYSIpQDVQTzZRH8BGml6.",
         "role":
             {
-                "granted_authorities": ["write"]
+                "granted_authorities": ["ROLE_ADMIN"]
             }
     }
 ]);
